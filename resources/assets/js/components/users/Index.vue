f030540b3beb58fc226e6afb7a26bde189e49fc2
OctoTech <info@octotech.de>
2016-11-07 19:38:28 +0100

    Code Cleanup

---
+++ resources/assets/js/components/users/Index.vue
@@ -0,0 +1,51 @@
+<template>
+    
+<table class="table">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="user in users">
+      <th></th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+
+</template>
+
+
+
+<script>
+
+    export default {
+        
+        data() {
+            
+            return {
+                users: []
+            };
+        },
+
+        ready: function() {
+            this.fetchUsers();
+        },
+
+        methods: {
+            fetchUsers: function() {
+                this.$http.get('/api/user').then((response) => {
+                    this.users = response.data
+                });
+            }
+        }
+
+    }
+
+</script>    
